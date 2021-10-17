0c85ed384582d328316eeb941e4cbb0a07f9156e
Rosamon <55803598+Rosamon@users.noreply.github.com>
2021-10-17 16:54:05 +0300

    We start our game! I'm create a main.cpp

---
+++ Git_2/main.cpp
@@ -0,0 +1,29 @@
+// main.cpp Главный файл с функцией запуска и оператором switch
+#include <iostream>
+using namespace std;
+int main()
+{
+	int choice = 0;
+	cout << "Please select an operation." << endl;
+	cout << "1 - \" +\"" << endl;
+	cout << "2 - \" +\"" << endl;
+	cout << "3 - \" +\"" << endl;
+	cout << "4 - \" +\"" << endl;
+	cin >> choice;
+	switch (choice)
+	{
+	case 1:
+		cout << "This feature is currently unavailable" << endl;
+		break;
+	case 2:
+		cout << "This feature is currently unavailable" << endl;
+		break;
+	case 3:
+		cout << "This feature is currently unavailable" << endl;
+		break;
+	case 4:
+		cout << "This feature is currently unavailable" << endl;
+		break;
+	}
+	return 0;
+}
